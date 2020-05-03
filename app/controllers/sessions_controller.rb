8ae1fefd63dc4e5f8e2df42f329f2ae27896c302
dannyflatiron <dannyflatiron@gmail.com>
2020-05-03 02:51:06 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+  end
+
+  def show
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
